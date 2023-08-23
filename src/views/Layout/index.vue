b7aa9d7e7f59170993b275a6fa6a3454c418dcb1
wu-yiqiang <wu_yiqiang@outlook.com>
2023-08-23 12:00:27 +0800

    清空

--- src/views/Layout/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <router-view></router-view>
-  <FooterBar />
-</template>
-<script lang="ts" setup>
-import FooterBar from '@/components/FooterBar/index.vue'
-</script>
-<style>
-</style>
